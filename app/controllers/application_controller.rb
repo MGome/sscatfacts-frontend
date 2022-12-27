a14db96d5381dae2492254a9f556af2e7fab4141
Mario Gomez <migomez6@uc.cl>
2022-12-27 04:15:44 -0300

    Add user authentication check on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticated_user?
+    !current_user.nil?
+  end
+
+  def verify_user_authentication
+    return redirect_to login_path unless authenticated_user?
+  end
 end
