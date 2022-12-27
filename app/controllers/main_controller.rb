019a31386642159af321071ded4787faeb2348a5
Mario Gomez <migomez6@uc.cl>
2022-12-27 04:47:15 -0300

    Add user authentication for index action on main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
+  before_action :verify_user_authentication, only: [:index]
+
   def home
+  end
+  
+  def index
+    @user = current_user
   end
 end
