7e38f0becaa16afe678015b81406b928a6e70440
Mario Gomez <migomez6@uc.cl>
2022-12-28 20:51:32 -0300

    Add session errors handle and display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
   def new
+    @errors = params[:errors]
   end
 
   def create
     response = HTTParty.get("#{ENV['BACKEND_URL']}/find_user", query: { 'username': params[:username] })
-    return redirect_to login_path if response['errors'].present?
+    return redirect_to login_path(errors: response['errors']) if response['errors'].present?
 
     session[:user_credentials] = response['body']
     redirect_to logged_index_path
