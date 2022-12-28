1db73a5eb990911523243ececc193fc04ffa5b16
Mario Gomez <migomez6@uc.cl>
2022-12-27 22:43:21 -0300

    Add liked fact query param to get fact endpoint

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,10 +6,11 @@ class MainController < ApplicationController
   
   def index
     @user = current_user
-    @get_fact_url = "#{ENV['BACKEND_URL']}/cat_fact"
+    @get_fact_url = "#{ENV['BACKEND_URL']}/cat_fact?user_id=#{@user.id }"
     response = HTTParty.get(@get_fact_url)
     @fact = response['body']['fact']
     @fact_id = response['body']['fact_id']
+    @liked_fact = response['body']['liked_fact']
     @like_fact_url = "#{ENV['BACKEND_URL']}/like_cat_fact"
   end
 end
