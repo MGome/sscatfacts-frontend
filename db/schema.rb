a59a7e63d176436111dabc752207015e0b783a9d
Mario Gomez <migomez6@uc.cl>
2022-12-28 05:39:46 -0300

    Delete unique index validation for username in order to use only backend information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_27_072445) do
+ActiveRecord::Schema.define(version: 2022_12_27_033342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2022_12_27_072445) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
