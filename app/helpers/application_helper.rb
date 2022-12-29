356b47e1bbe722f23943ba1857ba5a70e808595e
Mario Gomez <migomez6@uc.cl>
2022-12-28 22:17:26 -0300

    Add helper should_hide_navbar to hide navbar when is necesary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def should_hide_navbar(params)
+    (%w[users sessions].include?(params[:controller]) && params[:action] == 'new') ||
+      (params[:controller] == 'main' && params[:action] == 'home')
+  end
 end
